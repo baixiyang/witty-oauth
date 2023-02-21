e7d98d50da17d2f410323bf020a7910f16c525a8
baixiyang <baixy@isyscore.com>
2023-02-21 23:02:42 +0800

    feat: second

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import {bodyMiddleWare, mongodbWare, responseMiddleWare, startServer} from "witty-koa/src";
-startServer({
-    port: 5180,
+import {
+  bodyMiddleWare,
+  mongodbWare,
+  responseMiddleWare,
+  startServer,
+} from 'witty-koa/src';
+export function startOauth2Server({
+  mongodbUrl,
+  port = 5180,
+  rootUser = { username: 'root', password: '123456' },
+}: {
+  port: number;
+  mongodbUrl: string;
+  rootUser: { username: string; password: string };
+}) {
+  startServer({
+    port,
     controllers: [],
     middlewares: [
-        responseMiddleWare(),
-        bodyMiddleWare(),
-        mongodbWare({
-            url: 'xxx',
-            dbName: 'test',
-        }),
+      responseMiddleWare(),
+      bodyMiddleWare(),
+      mongodbWare({
+        url: mongodbUrl,
+        dbName: 'witty-oauth2',
+      }),
     ],
-});
+  });
+}
